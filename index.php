ed59f063d0a06d1bd82a69a16c16312d71849765
Aakash <vermaaakash1234@gmail.com>
2016-05-26 12:41:38 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+</head>
+<body>
+    <div class="outerbox">
+    // This is the outerbox
+    </div>
+</body>
+</html>
