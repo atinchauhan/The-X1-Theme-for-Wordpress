63d5ebbfeb235c0b822effe3d7faae0b06a476d2
Aakash <vermaaakash1234@gmail.com>
2016-05-27 17:18:14 +0530

    Static background , post completed , bug fixes

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <div class="outerbox">
     <!--It is the outerbox surrounding everything-->
+    <div class="foreffect">
     <?php get_header(); ?>
+    </div>
     <div class="Content">
         <div class="post-area">
         <?php /* Start the Loop */ ?>
           <?php while ( have_posts() ) : the_post(); ?>
-                      <div class="posts">
-                <div class="title-post">
-                    <?php echo get_avatar(get_the_author_meta( 'ID' ),                          40);?>&nbsp;&nbsp;<?php the_title()?>
-                    <?php the_content("Looks Interesting? Wanna Read More ?"); ?>
-                    </div>
+                    <div class="posts">
+                        <div class="title-post">
+                            <?php echo get_avatar(get_the_author_meta( 'ID' ),120);?>
+                            <?php the_title()?>
+                        </div>
+                        <div class="post-content">
+                            <?php the_content("Read More "); ?>
+                        </div>
                     </div>
                     <?php endwhile; ?>
         </div>
