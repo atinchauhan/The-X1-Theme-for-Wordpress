6c5e97ca17242db4fa1c9d7ba1738bfee2aa4ca2
Aakash <vermaaakash1234@gmail.com>
2016-07-09 22:09:27 +0530

    alpha stage over, beta started

---
+++ 404.php
@@ -0,0 +1,18 @@
+<div class="outerbox">
+<div class="foreffect">
+<?php get_header(); ?>
+</div>
+
+    <div class="not-found-area">
+        <div class="not-found-text">
+        <h1 id="not-found-text">Sorry Buddy :(<br> But there is Nothing Here</h1>
+        </div>
+        <div class="not-found-extra">
+        <h1><a href="<?php echo home_url( '/' ); ?>" id="not-found-home-button">Let's Go Home</a></h1><br>
+        <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+        <span id="tags">Tags:- &nbsp;&nbsp;&nbsp;&nbsp;<?php wp_tag_cloud( 'smallest=18' ); ?></span><br>
+        <?php get_search_form() ; ?>
+        </div>
+    </div>
+<?php get_footer(); ?>
+</div>
